74f43ece65633233a5964f22abd973fec1275c05
H3rsh3 <hersh247@gmail.com>
2024-11-19 18:47:57 -0600

    certvalidation

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ console.log("running")
 chrome.webRequest.onErrorOccurred.addListener(
   function(details) {
     console.log(details.statuCode, details.error, details.type)
-    if (details.type === "main_frame" && details.error === 'net::ERR_CONNECTION_RESET') {
+    if (details.type === "main_frame" && (details.error === 'net::ERR_CONNECTION_RESET' || details.error === 'net::ERR_CERT_AUTHORITIY_INVALID')) {
         // Get the custom URL from storage
         chrome.storage.sync.get(['customUrl'], function(data) {
           let redirectUrl = data.customUrl || chrome.runtime.getURL("oops.html"); // Fallback to oops.html
